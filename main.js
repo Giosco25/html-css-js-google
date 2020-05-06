d6a19fb3f5d7a7e09294959b40b77395d5468d2d
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-06 15:38:36 +0200

    Revisione JS

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
 
     $('.link-icon > li > i').click(function(){
         console.log('click');
-        var menu_icona = $(this).next('.dropdown')
+        var menu_icona = $(this).next('.dropdown').show();
         console.log(menu_icona);
 
-        
+    if (menu_icona.is(':visible')) {
+        menu_icona.hide();
+
+    }else {
+        $('.dropdown').hide();
+        menu_icona.show();
+    }
+
     });
 
 
