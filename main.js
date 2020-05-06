edf507930e157f5a3e63462affc767056fd1f28f
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-06 15:13:45 +0200

    Inizio JS

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.link-icon > li > i').click(function(){
+        console.log('click');
+        var menu_icona = $(this).next('.dropdown')
+        console.log(menu_icona);
+
+        
+    });
+
+
+
+
+
+})
